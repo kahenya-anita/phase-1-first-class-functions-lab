afc353adcf83c5e1ae69e70ddd3b5f4f625333e1
kahenya-anita <anitakahenya1@gmail.com>
2023-03-23 10:37:27 +0300

    Fare multiplier

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution in this file!
+function createFareMultiplier(multiplier) {
+  const fareMultiplier = (fare) => fare * multiplier;
+  return fareMultiplier();
+}
+createFareMultiplier(2);
